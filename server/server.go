f23919d8c73b2f8c750bab30e7d56ce03e9e18ce
Alberto Arancibia <aarancib@gmail.com>
2022-01-13 01:04:11 -0300

    Se agregan nuevos métodos de respuesta.

--- server/server.go
+++ server/server.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"quasar-fire/utils"
-	"reflect"
+	"strings"
 	"sync"
 )
 
@@ -79,7 +79,7 @@ func NewSignalHandlers() *SignalHandlers {
 		},
 	}
 
-	fmt.Println(signals)
+	//fmt.Println(signals)
 
 	return &SignalHandlers{
 		Data: signals,
@@ -94,8 +94,7 @@ func (h *SignalHandlers) get(w http.ResponseWriter, r *http.Request) {
 	//Bloquea ingreso de nuevos datos para obtener valores actuales
 	h.Lock()
 	for _, signal := range h.Data {
-		fmt.Println(reflect.TypeOf(signal), signal)
-
+		//fmt.Println(reflect.TypeOf(signal), signal)
 		signals = append(signals, signal)
 	}
 	h.Unlock()
@@ -130,7 +129,6 @@ func (h *SignalHandlers) post(w http.ResponseWriter, r *http.Request) {
 
 	var signal Signal
 	err = json.Unmarshal(bodyBytes, &signal)
-	//fmt.Println(coaster) debug decorded json
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Println(err)
@@ -141,6 +139,48 @@ func (h *SignalHandlers) post(w http.ResponseWriter, r *http.Request) {
 	h.Lock()
 	h.Data = append(h.Data, signal)
 	h.Unlock()
+}
+
+//Obtiene listado de satelites ingresados
+func (h *SignalHandlers) GetSatellites() {
+	//TODO: implementar con objeto global o parámetro
+	//h.Sats = append(h.Sats, sats)
+}
+
+//TODO: Agrega multiples objetos Signal al listado.
+func (h *SignalHandlers) postSignals(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	//validates the content-type from the post
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(fmt.Sprintf("need content-type 'application/json', but got '%s'", ct)))
+		return
+	}
+
+	fmt.Println(bodyBytes)
+	var signals []Signal
+	err = json.Unmarshal(bodyBytes, &signals)
+	//fmt.Println(signals) //debug decoded json
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(err)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	/*
+		h.Lock()
+		h.Data = append(h.Data, signal)
+		h.Unlock()
+	*/
 }
 
 //Lista o agrega un objeto signal según método HTTP usado: GET, POST
@@ -154,8 +194,83 @@ func (h *SignalHandlers) Signals(w http.ResponseWriter, r *http.Request) {
 		return
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Method not allowed"))
+		w.Write([]byte("Método no permitido."))
 	}
+}
+
+func (h *SignalHandlers) getDistance(w http.ResponseWriter, r *http.Request) {
+
+	//Obtener distancias y mensajes de h si existen
+
+	r1 := 100.0
+	r2 := 115.5
+	r3 := 142.7
+
+	x, y := utils.GetLocation(float32(r1), float32(r2), float32(r3))
+
+	m1 := []string{"este", "", "", "mensaje", ""}
+	m2 := []string{"", "es", "", "", "secreto"}
+	m3 := []string{"este", "", "un", "", ""}
+
+	messages := [][]string{m1, m2, m3}
+	message := utils.GetMessage(messages)
+
+	//En caso que no existan suficientes puntos
+	if len(messages) < 3 {
+		w.Header().Add("Content-type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404: Not Found."))
+	}
+
+	pos := utils.SecretPosition{
+		Point:   utils.Point{X: float64(x), Y: float64(y)},
+		Message: message,
+	}
+
+	jsonBytes, err := json.Marshal(pos)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+
+}
+
+//Interactúa con REST API usando múltiples objetos.
+//POST: los guarda en SignalHandlers
+//GET: intenta calcular la ubicación de la nava si hay datos suficientes
+func (h *SignalHandlers) SignalsMultiple(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		h.getDistance(w, r)
+		return
+	case "POST":
+		h.postSignals(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Método no permitido."))
+	}
+}
+
+//TODO: Agrega multiples objetos Signal al listado.
+func (h *SignalHandlers) SignalSat(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.String(), "/")
+
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	sat_name := "kenobi"
+	//compara parametro
+	if parts[2] == sat_name {
+		h.Signals(w, r)
+		return
+	}
+
 }
 
 func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
